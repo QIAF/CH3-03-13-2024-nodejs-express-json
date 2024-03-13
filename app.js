e9e557f39891d65da8b1f4faa8e9d91533138d1a
QIAF <qonitaafifah3@gmail.com>
2024-03-14 01:33:29 +0700

    nodejs-express-json

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-// midleware  (JANGAN SAMPE KELEWAT)
-app.use(express.json)
+// midleware untuk membaca require JSON dari request body (JANGAN SAMPE KELEWAT)
+app.use(express.json());
+
 // read file jsonnya
 const customers = JSON.parse(
     fs.readFileSync(`${__dirname}/data/dummy.json`)
@@ -19,32 +20,38 @@ app.get("/", (req, res, next) => {
 app.get("/api/v1/customers", (req, res, next) => {
     res.status(200).json({
     status:"success",
-    totalData:customers.length,
+    totalData: customers.length,
     data:{
-        customers,
+        customers: customers,
     },
-})
+});
 });
 // create data baru
 app.post("/api/v1/customers", (req, res) => {
     console.log(req.body);
 
-    const newCostumer = req.body;
-
-    costumers.push(newCustomer);
-
+    costumers.push(req.body);
     fs.writeFile(`${__dirname}/data/dummy.json`,
     JSON.stringify(costumers),
     (err) => {
+        if (err) {
+            res.status(500).json({
+                status:"error",
+                message:"Gagal menyimpan",
+            });
+        }else{
         res.status(201).json({
             status : "success",
             data :{
-                costumer: newCustomer,
+                costumer: notStrictEqual.body,
             },
         });
+
+        }
+
     }
 );
-    res.send("oke udah");
+    // res.send("oke udah");
 })
 
 app.listen(PORT, () => {
